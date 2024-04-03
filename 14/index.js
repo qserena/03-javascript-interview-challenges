9b0720a1fc8f78b12175f5abd914e0c6569e0fca
qserena <tom@serenander.se>
2024-04-03 17:42:55 +0200

    No 14

---
+++ 14/index.js
@@ -0,0 +1,28 @@
+/*
+14 - Is it an Anagram?
+
+Anagrams are groups of words that can be spelled with the same letters. For example, the letters in "pea" can be rearrange to spell "ape", and the letters in "allergy" can be rearranged to spell "gallery."
+
+Write a function to check if two strings of lowercase letters are anagrams. Return true if the word is an anagram. Return false if it isn't.
+
+Example input: "allergy", "gallery"
+Example output: true
+
+Example input: "rainbow", "crossbow"
+Example output: false
+*/
+
+function isAnagram(str1, str2) {
+	if (str1.length !== str2.length) {
+		return false
+	}
+	return str1.split('').sort().join('') === str2.split('').sort().join('')
+}
+
+console.log(isAnagram('allergy', 'gallery'))
+console.log(isAnagram('treasure', 'measure'))
+console.log(isAnagram('inch', 'chin'))
+console.log(isAnagram('rainbow', 'crossbow'))
+console.log(isAnagram('rainbow', 'rainbo'))
+console.log(isAnagram('rainbo', 'rainbow'))
+console.log(isAnagram('rainbo', 'rainbu'))
