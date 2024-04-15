52dec5d4abaa06d9bd97480fbdf746954b785079
qserena <tom@serenander.se>
2024-04-15 10:51:26 +0200

    Cosmetics.

--- 18/index.js
+++ 18/index.js
@@ -1,14 +1,18 @@
-// 18 - Palindromes
+/*
 
-// Palindromes are words that are the same forward or backward. For example, the words "noon" and "kayak" are palindromes.
+18 - Palindromes
 
-// Write a function to check if a lowercased string of letters is a palindrome. If the word is palindrome, return true. If it isn't, return false.
+Palindromes are words that are the same forward or backward. For example, the words "noon" and "kayak" are palindromes.
 
-// Example input: "motorbike"
-// Example output: false
+Write a function to check if a lowercased string of letters is a palindrome. If the word is palindrome, return true. If it isn't, return false.
 
-// Example input: "rotator"
-// Example output: true
+Example input: "motorbike"
+Example output: false
+
+Example input: "rotator"
+Example output: true
+
+*/
 
 function isPalindrome(str) {
     return str === str.split('').reverse().join('')
