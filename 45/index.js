b19156251da2264e31e1597bea613d058ae599be
qserena <tom@serenander.se>
2024-04-19 13:51:44 +0200

    No 45 solved.

--- 45/index.js
+++ 45/index.js
@@ -18,6 +18,37 @@ Expected output:
 
 */
 
-function getUniqueTags(data) {}
+function getUniqueTags(data) {
+    let allTags = []
+    data.forEach((show) => {
+        allTags = [...allTags, ...show.tags]
+    })
+    const uniqueTags = []
+    allTags.forEach((item) => {
+        if (!uniqueTags.includes(item)) {
+            uniqueTags.push(item)
+        }
+    })
+    return uniqueTags
+}
+
+//Alternative solution: Avoid nested loops (increase performance)
+
+// function getUniqueTags(data) {
+//     const tags = data.map((podcast) => podcast.tags).flat()
+//     const uniqueTags = {}
+
+//     // filter tags arr
+//     return tags.filter((tag) => {
+//         // look up the tag in the uniqueTags obj
+//         if (!uniqueTags[tag]) {
+//             uniqueTags[tag] = true
+//             // if it's not the, we have a unique item
+//             // put the item in our object with a value of true
+//             return true
+//         }
+//         return false
+//     })
+// }
 
 console.log(getUniqueTags(mediaData))
